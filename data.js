8ec2012281cb9f9e856f3db5b7ddd0572e5ec54c
Fante449 <tim@mail-helber.de>
2025-12-27 13:20:34 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: "dhn-rose",
+    name: "DHN Rosé",
+    price_private: 12.99,
+    price_gastro: 10.99
+  },
+  {
+    id: "dhn-rose-abo",
+    name: "DHN Rosé Abo",
+    price_private: 11.49,
+    price_gastro: 9.99
+  },
+  {
+    id: "dhn-blanc",
+    name: "DHN Blanc",
+    price_private: 13.99,
+    price_gastro: 11.99
+  },
+  {
+    id: "dhn-blanc-abo",
+    name: "DHN Blanc Abo",
+    price_private: 12.49,
+    price_gastro: 10.49
+  }
+];
